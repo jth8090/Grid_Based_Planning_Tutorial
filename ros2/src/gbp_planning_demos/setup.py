b8a61e517d4efb1ffa400e12b40e5d361c87058f
Taehyun Jung <jth8090@khu.ac.kr>
2025-11-20 23:01:34 +0900

    Update maintainer info and package description
    Updated maintainer information and simplified description.

--- ros2/src/gbp_planning_demos/setup.py
+++ ros2/src/gbp_planning_demos/setup.py
@@ -14,24 +14,20 @@ setup(
         f'{package_name}.nodes',
     ],
     data_files=[
-        # ament index 등록
-        ('share/ament_index/resource_index/packages',
+        ('share/ament_index/resource_index/packages', 
          ['resource/' + package_name]),
-        # package.xml
         ('share/' + package_name, ['package.xml']),
-        # launch 파일 설치
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='YOUR_NAME',
-    maintainer_email='you@example.com',
-    description='Grid-based planning demos (DFS, BFS, Dijkstra, A*) on ROS2 OccupancyGrid.',
+    maintainer='Taehyun Jung',
+    maintainer_email='jth8090@khu.ac.kr',
+    description='Grid-based planning demos',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # ros2 run gbp_planning_demos simple_grid_planner
             'simple_grid_planner = gbp_planning_demos.nodes.simple_grid_planner:main',
         ],
     },
